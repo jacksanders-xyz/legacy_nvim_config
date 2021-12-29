75ebb68b1616ec19f1fabc94cf4c5ea4c121d843
Jack Sanders <Guitarbreakerjs@gmail.com>
2021-12-29 00:05:41 -0700

    symbols work

--- lua/jacksvimlua/SCORE_MODE_MODULES/scoreMaps.lua
+++ lua/jacksvimlua/SCORE_MODE_MODULES/scoreMaps.lua
@@ -22,5 +22,16 @@ return {
         ['noremap'] = true,
         ['silent'] = true
       },
+      -- make a black note
+      -- ['n'] = {
+      --   ['rhs'] = 'i<C-v>Ue0a4<CR><Esc>',
+      --   ['noremap'] = true,
+      --   ['silent'] = true
+      -- },
+      ['n'] = {
+        ['rhs'] = 'i<Esc>h',
+        ['noremap'] = true,
+        ['silent'] = true
+      },
     }
 }
